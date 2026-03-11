5337ce44d716cede9decfc5986a4ff9d1c2e9a7f
kaviyarashup <kaviyarashupkiaq@gmail.com>
2026-03-11 07:59:57 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+const apiRoutes = require("./routes/api")
+
+app.use("/api", apiRoutes)
+
+app.use(express.static(path.join(__dirname, "public")))
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+  console.log("Server running on port 3000")
+})
