a05b387880276d6e7bafe23c8cff65cb7906dc36
root <root@DESKTOP-1VU94AL.localdomain>
2026-03-12 10:22:30 +0000

    Add home route

--- server.js
+++ server.js
@@ -1,12 +1,3 @@
-const express = require('express');
-const app = express();
-
- app.get('/', (req, res) => {
-   res.send('<h1>Success!</h1><p>Your application is now hosting live on EC2.</p>');
-   });
-
-   
-   const PORT = 5000;
-   app.listen(PORT, '0.0.0.0', () => {
-     console.log(`Server running on port ${PORT}`);
-     }); 
+app.get('/', (req, res) => {
+	    res.send('<h1>Deployment 100% Successful!</h1>');
+});
