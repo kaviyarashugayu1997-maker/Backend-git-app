3fb9e9490b5f0772a641dbd7238a678be28515fc
root <root@DESKTOP-1VU94AL.localdomain>
2026-03-12 09:58:21 +0000

    update server.js

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-const express = require("express")
-const path = require("path")
-
-const app = express()
-
-const apiRoutes = require("./routes/api")
-
-app.use("/api", apiRoutes)
-
-app.use(express.static(path.join(__dirname, "public")))
-
-const PORT = 5000
-
-app.listen(PORT, () => {
-  console.log("Server running on port 5000")
-})
+const PORT = 5000;
+// Using '0.0.0.0' tells the app to accept external connections
+// app.listen(PORT, '0.0.0.0', () => {
+//     console.log(`Server is running on http://13.63.137.41:${PORT}`);
+//     });
+//
+//     // Add this so the browser doesn't show "Cannot GET /"
+//     app.get('/', (req, res) => {
+//         res.send('<h1>The Deployment Worked!</h1><p>Your app is now live on EC2.</p>');
+//         });
