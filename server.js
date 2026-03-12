8b66d4ae3c29e1c8898c5b36a14f562498d33b3d
root <root@DESKTOP-1VU94AL.localdomain>
2026-03-12 10:18:40 +0000

    Fix: Added home route to remove Cannot GET error

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const PORT = 5000;
-// Using '0.0.0.0' tells the app to accept external connections
-// app.listen(PORT, '0.0.0.0', () => {
-//     console.log(`Server is running on http://13.63.137.41:${PORT}`);
-//     });
-//
-//     // Add this so the browser doesn't show "Cannot GET /"
-//     app.get('/', (req, res) => {
-//         res.send('<h1>The Deployment Worked!</h1><p>Your app is now live on EC2.</p>');
-//         });
+const express = require('express');
+const app = express();
+
+ app.get('/', (req, res) => {
+   res.send('<h1>Success!</h1><p>Your application is now hosting live on EC2.</p>');
+   });
+
+   
+   const PORT = 5000;
+   app.listen(PORT, '0.0.0.0', () => {
+     console.log(`Server running on port ${PORT}`);
+     }); 
